7071f6501d3a70b62ffab3d4059103a7c120db9f
thiago-figueredo <thiago16fca@outlook.com>
2023-09-23 18:48:59 -0300

    feat: remove comments and and time library

--- src/wutils.c
+++ src/wutils.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <winsock2.h>
 #include <windows.h>
 #include "./wutils.h"
--- src/wutils.h
+++ src/wutils.h
@@ -5,18 +5,18 @@
 #include <string.h>
 #include <assert.h>
 #include <errno.h>
-#include <winsock2.h>  // Include Windows socket header
+#include <winsock2.h>
 
 #define SERVER_PORT 8080
 #define BUFFER_SIZE 6
 #define MAX_PENDING_CONNECTIONS 2
 #define MAX_DATE_TIME_LENGTH 20
 
-void close_socket(SOCKET fd); // Change int to SOCKET
-void close_and_die(SOCKET fd); // Change int to SOCKET
+void close_socket(SOCKET fd);
+void close_and_die(SOCKET fd)
 bool handle_error(int condition, const char* msg);
-void try_respond(SOCKET socket_fd, char* buffer, int max_buffer_size, char* msg); // Change long to int
-void try_send(SOCKET socket_fd, char* buffer); // Change long to int
+void try_respond(SOCKET socket_fd, char* buffer, int max_buffer_size, char* msg);
+void try_send(SOCKET socket_fd, char* buffer);
 
 void panic(const char* msg);
 void hexdump(const char* data);
